b7bcaa44a902042be3dc7c92699d71de6eb4c229
Merlinaphist <82560988+Merlinaphist@users.noreply.github.com>
2022-11-27 03:14:25 +0800

    Delete 1.1.blast.bash

--- 1.1.blast.bash
+++
@@ -1,4 +0,0 @@
-cd blast
-makeblastdb -dbtype nucl -in marker.fasta -out test
-blastn -db test -query cleaned_reads.fasta -task blastn-short -outfmt "6 qseqid qlen qstart qend stitle slen sstart send pident bitscore evalue" -out output.tsv -max_hsps 1
-echo -e "qseqid\tqlen\tqstart\tqend\tstitle\tslen\tsstart\tsend\tpident\tbitscore\tevalue" | cat - output.tsv > results.tsv
