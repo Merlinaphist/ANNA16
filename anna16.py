0e61f4504612110aeec35cdf92249b0c36d12025
Merlinaphist <mjz201126@outlook.com>
2023-04-19 12:01:00 +0800

    Update anna16.py

--- anna16.py
+++ anna16.py
@@ -14,6 +14,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import Ridge, Lasso, LinearRegression
 from sklearn.svm import SVR
 import os
+import ZipFile
 
 class Preprocessing():
     def __init__(self,k_size=6):
