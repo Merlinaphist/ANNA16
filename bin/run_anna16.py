2fa22c00f7d05411210d63e8df60979b766d5843
Merlinaphist <mjz201126@outlook.com>
2025-03-04 16:55:36 -0500

    Update run_anna16.py

--- bin/run_anna16.py
+++ bin/run_anna16.py
@@ -21,70 +21,48 @@ model = CopyNumberPredictor(region=args.r)
 model.load(args.t == "True")
 pp = Preprocessing()
 
-
-
-def predict_from_fasta(args):
-    input_name, output_name = args
-    input_file = open(input_name,"r")
-    output_file = open(output_name, 'w')
-    output_file.write("index,predicted_copy_number\n")
-    tmp_seq = ""
-    for line in input_file:
-        if line[0] != ">":
-            tmp_seq+=line.split("\n")[0]
-        else:
-            if tmp_seq != "":
-                X = pp.CountKmers([tmp_seq])
-                pred = model.predict(X)[0]
-                output_file.write(f"{seqid},{pred}\n")
-            tmp_seq = ""
-            seqid = line.split("\n")[0][1:]       
-    input_file.close()
-    output_file.close()
-
-
-for i in range(len(args.i[0])):
-    input_name = args.i[0][i]
-    if input_name.split(".")[-1] not in ["fasta","fna","fa"]:
-        raise ValueError('Invalid file format. Expected formats are ["fasta","fna","fa"].')
-    if args.o == None:
-        suffix = input_name.split(".")[0]
-        output_name = suffix+".csv"
-    else:
-        output_name = args.o[0][i]+".csv"
-    predict_from_fasta([input_name, output_name])
-
-
-# #Input FASTA files
-# uploaded_seqs = {}
-# for filename in args.i[0]:
-#     if filename.split(".")[-1] not in ["fasta","fna","fa"]:
+# for i in range(len(args.i[0])):
+#     input_name = args.i[0][i]
+#     if input_name.split(".")[-1] not in ["fasta","fna","fa"]:
 #         raise ValueError('Invalid file format. Expected formats are ["fasta","fna","fa"].')
-#     else:
-#         uploaded_seqs[filename] = pp.ReadFASTA(filename)
-
-# #Run Predictions
-# task_names = list(uploaded_seqs.keys())
-# results = {}
-# for task_name in task_names:
-#     X = pp.CountKmers(uploaded_seqs[task_name])
-#     results[task_name] = model.predict(X)
-#     results[task_name] = pd.DataFrame(results[task_name],
-#                                       index = uploaded_seqs[task_name].index,
-#                                       columns = ["predicted_copy_number"])
-
-# #Save Prediction Results
-# for i in range(len(task_names)):
-#     task_name = task_names[i]
 #     if args.o == None:
-#         suffix = filename.split(".")[-1]
-#         downloaded_filename = task_name.split("."+suffix)[0]+".csv"
+#         suffix = input_name.split(".")[0]
+#         output_name = suffix+".csv"
 #     else:
-#         downloaded_filename = args.o[0][i]+".csv"
-#     values = results[task_name]["predicted_copy_number"].values.tolist()
-#     indices = results[task_name].index.tolist()
-#     with open(downloaded_filename, 'w') as f:
-#         f.write('index,predicted_copy_number\n')
-#         for i in range(len(values)):
-#             line = str(indices[i])+","+str(values[i])+"\n"
-#             f.write(line)
+#         output_name = args.o[0][i]+".csv"
+#     predict_from_fasta(input_name, output_name, model)
+
+
+#Input FASTA files
+uploaded_seqs = {}
+for filename in args.i[0]:
+    if filename.split(".")[-1] not in ["fasta","fna","fa"]:
+        raise ValueError('Invalid file format. Expected formats are ["fasta","fna","fa"].')
+    else:
+        uploaded_seqs[filename] = pp.ReadFASTA(filename)
+
+#Run Predictions
+task_names = list(uploaded_seqs.keys())
+results = {}
+for task_name in task_names:
+    X = pp.CountKmers(uploaded_seqs[task_name])
+    results[task_name] = model.predict(X)
+    results[task_name] = pd.DataFrame(results[task_name],
+                                      index = uploaded_seqs[task_name].index,
+                                      columns = ["predicted_copy_number"])
+
+#Save Prediction Results
+for i in range(len(task_names)):
+    task_name = task_names[i]
+    if args.o == None:
+        suffix = filename.split(".")[-1]
+        downloaded_filename = task_name.split("."+suffix)[0]+".csv"
+    else:
+        downloaded_filename = args.o[0][i]+".csv"
+    values = results[task_name]["predicted_copy_number"].values.tolist()
+    indices = results[task_name].index.tolist()
+    with open(downloaded_filename, 'w') as f:
+        f.write('index,predicted_copy_number\n')
+        for i in range(len(values)):
+            line = str(indices[i])+","+str(values[i])+"\n"
+            f.write(line)
