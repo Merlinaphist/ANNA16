a97813fadb018bd4482390cbcd834d405dcdb81c
Merlinaphist <82560988+Merlinaphist@users.noreply.github.com>
2026-01-15 14:22:52 -0500

    Update run_anna16.py

--- bin/run_anna16.py
+++ bin/run_anna16.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from anna16 import Preprocessing, CopyNumberPredictor #, predict_from_fasta
+from anna16 import Preprocessing, get_model #, predict_from_fasta
 import pandas as pd
 import numpy as np
 import warnings
@@ -9,31 +9,20 @@ import os
 # from multiprocessing import Pool
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', type=str, choices=["full_length", "V1-V2", "V1-V3", "V3-V4", "V4-V5", "V4", "V6-V8", "V7-V9"], help='Target Region', required=True)
-parser.add_argument('-t', type=str, choices=["True", "False"], help='Primers Trimmed', required=True)
-parser.add_argument('-i', action='append', nargs='+', help='Input File', required=True)
-parser.add_argument('-o', action='append', nargs='*', help='Output File')
+parser.add_argument('-r', '--region', type=str, choices=["full_length", "V1-V2", "V1-V3", "V3-V4", "V4-V5", "V4", "V6-V8", "V7-V9"], help='Target Region', required=True)
+parser.add_argument('-m', '--ml_type', type=str, choices=["cuml", "sklearn"], help='Machine Learning Framework Type', default="cuml")
+parser.add_argument('-i', '--input', action='append', nargs='+', help='Input File', required=True)
+parser.add_argument('-o', '--output', action='append', nargs='*', help='Output File')
 # parser.add_argument('--streaming', help='Process the input file sequence by sequence', action="store_true")
 
 args = parser.parse_args()
 
 #Initialize the Model
-model = CopyNumberPredictor(region=args.r)
-model.load(args.t == "True")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_dir = os.path.join(current_dir, "model_files")
+model = get_model(ml_type=args.ml_type)
+model.load(file_prefix=f"{model_dir}/{args.region}/{args.region}")
 pp = Preprocessing()
-
-# if args.streaming:
-#     for i in range(len(args.i[0])):
-#         input_name = args.i[0][i]
-#         if input_name.split(".")[-1] not in ["fasta","fna","fa"]:
-#             raise ValueError('Invalid file format. Expected formats are ["fasta","fna","fa"].')
-#         if args.o == None:
-#             suffix = input_name.split(".")[0]
-#             output_name = suffix+".csv"
-#         else:
-#             output_name = args.o[0][i]+".csv"
-#         predict_from_fasta(input_name, output_name, model)
-
 
 #Input FASTA files
 uploaded_seqs = {}
@@ -61,10 +50,4 @@ for i in range(len(task_names)):
         downloaded_filename = task_name.split("."+suffix)[0]+".csv"
     else:
         downloaded_filename = args.o[0][i]+".csv"
-    values = results[task_name]["predicted_copy_number"].values.tolist()
-    indices = results[task_name].index.tolist()
-    with open(downloaded_filename, 'w') as f:
-        f.write('index,predicted_copy_number\n')
-        for i in range(len(values)):
-            line = str(indices[i])+","+str(values[i])+"\n"
-            f.write(line)
+    output_file = results[[task_name]].to_csv(downloaded_filename, index=True)
