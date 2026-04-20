92e90494cd212b28137c42e989e31684000038d6
Merlinaphist <82560988+Merlinaphist@users.noreply.github.com>
2026-04-20 19:09:41 -0400

    Update __init__.py

--- src/anna16/__init__.py
+++ src/anna16/__init__.py
@@ -39,7 +39,7 @@ class Preprocessing():
         if type(seqs) in [type([]),type(pd.core.series.Series([1]))]:
             with Pool(cpu_count()) as p:
                 kmer = p.map(self.seq2kmer, seqs)
-            # kmer = pd.Series(seqs).apply(lambda x: self.seq2kmer(x, self.k_size))
+            # kmer = pd.Series(seqs).apply(lambda x: self.seq2kmer(x))
             transformed_X = self.vectorizer.transform(kmer).toarray()
             return transformed_X
         else:
